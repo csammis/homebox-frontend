b12d71a5750f69a632a198c820b32e9936cec10a
Chris Sammis <csammis@gmail.com>
2026-08-15 18:55:13 -0500

    Change to web history for routing

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
-import { createRouter, createMemoryHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomeScreen from "./components/HomeScreen.vue";
 import ItemDetails from "./components/ItemDetails.vue";
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes: [
     { path: "/", component: HomeScreen },
     { path: "/details/:id", component: ItemDetails, props: true },
