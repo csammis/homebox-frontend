c2ccb3c3a3d3aec46f5e7bd22b7dc25fe122a0c1
csammis <csammis@gmail.com>
2026-08-26 03:33:22 +0000

    Removed distutils dependency

--- request_proxy/gunicorn.py
+++ request_proxy/gunicorn.py
@@ -1,8 +1,6 @@
 import multiprocessing
 import os
 
-from distutils.util import strtobool
-
 bind = f"0.0.0.0:{os.getenv('PORT', '8765')}"
 accesslog = "-"
 access_log_format = (
@@ -12,6 +10,6 @@ access_log_format = (
 workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
 threads = int(os.getenv("PYTHON_MAX_THREADS", "1"))
 
-reload = bool(strtobool(os.getenv("WEB_RELOAD", "false")))
+reload = bool(False if os.getenv("WEB_RELOAD", "false") in ('n', 'no', 'f', 'false', 'off', '0') else True)
 
 timeout = int(os.getenv("WEB_TIMEOUT", "120"))
